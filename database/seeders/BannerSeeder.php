dfee7e3d0f0f1ec9188a5d936f6a00db22da5de2
mojtaba <mojtaba.kh.sh@gmail.com>
2021-06-17 16:38:06 +0430

    fix some bugs

--- database/seeders/BannerSeeder.php
+++ database/seeders/BannerSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Banner;
+use App\Category;
 use Illuminate\Database\Seeder;
 
 class BannerSeeder extends Seeder
@@ -16,9 +17,11 @@ class BannerSeeder extends Seeder
     {
         Banner::truncate();
 
+        $categories = Category::has('products')->where('parent_id','!=',0)->get()->random(6);
+
         Banner::factory()->create([
             'title' => 'Women’s fashion',
-            'url' => 'google.com',
+            'url' => 'category/' . $categories[0]->slug,
             'image' => 'upload/faker/left-banner.jpg',
             'position' => 'left',
             'description' => 'Sitamet, consectetur adipiscing elit, sed do eiusmod tempor incidid-unt labore edolore magna aliquapendisse ultrices gravida.',
@@ -26,7 +29,7 @@ class BannerSeeder extends Seeder
 
         Banner::factory()->create([
             'title' => 'Men’s fashion',
-            'url' => 'google.com',
+            'url' => 'category/' . $categories[1]->slug,
             'image' => 'upload/faker/1600866707-category-2.jpg',
             'position' => 'right',
             'description' => '',
@@ -34,7 +37,7 @@ class BannerSeeder extends Seeder
 
         Banner::factory()->create([
             'title' => 'Men’s fashion',
-            'url' => 'google.com',
+            'url' => 'category/' . $categories[2]->slug,
             'image' => 'upload/faker/1600866707-category-2.jpg',
             'position' => 'right',
             'description' => '',
@@ -42,7 +45,7 @@ class BannerSeeder extends Seeder
 
         Banner::factory()->create([
             'title' => 'Kid’s fashion',
-            'url' => 'google.com',
+            'url' => 'category/' . $categories[3]->slug,
             'image' => 'upload/faker/1600867792-category-3.jpg',
             'position' => 'right',
             'description' => '',
@@ -50,7 +53,7 @@ class BannerSeeder extends Seeder
 
         Banner::factory()->create([
             'title' => 'Cosmetics',
-            'url' => 'google.com',
+            'url' => 'category/' . $categories[4]->slug,
             'image' => 'upload/faker/1600867818-category-4.jpg',
             'position' => 'right',
             'description' => '',
@@ -58,7 +61,7 @@ class BannerSeeder extends Seeder
 
         Banner::factory()->create([
             'title' => 'Accessories',
-            'url' => 'google.com',
+            'url' => 'category/' . $categories[5]->slug,
             'image' => 'upload/faker/1600867966-category-5',
             'position' => 'right',
             'description' => '',
